61f3c22ad9141ee5fd26fbb16aa1e8cbab6b34cf
codingMaz <m.balandari.toroghi@gmail.com>
2024-09-25 14:18:27 +0330

    Add project files.

---
+++ MatchingGame/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MatchingGame
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MatchingGame/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace MatchingGame
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayAgainButton_Clicked(object sender, EventArgs e)
+        {
+            AnimalButtons.IsVisible = true;
+            PlayAgainButton.IsVisible = false;
+
+            List<string> animalEmoji = [
+                "🐈", "🐈", "🐶", "🐶", "🐔", "🐔", "🐯", "🐯",
+                "🐒", "🐒", "🦊", "🦊", "🐘", "🐘", "🦈", "🦈",
+            ];
+
+            foreach (var button in AnimalButtons.Children.OfType<Button>())
+            {
+                int index = Random.Shared.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                button.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
